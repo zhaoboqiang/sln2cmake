565b8906b08971e7021803252282f37e5ca9bbd6
Zhao Boqiang <zhaoboqiang@queencrowd.com>
2016-05-14 22:03:54 +0800

    Add progress and project name in statusbar.

--- sln2cmake/SolutionConverter.cs
+++ sln2cmake/SolutionConverter.cs
@@ -14,13 +14,25 @@ namespace Sln2CMake
     {
         static public void Run(IServiceProvider serviceProvider, IVsStatusbar statusbar)
         {
+            uint cookie = 0;
+
             var dte = (DTE2)serviceProvider.GetService(typeof(DTE));
             var projects = dte.Solution.Projects;
-            for (int i = 1, n = projects.Count; i <= n; ++i)
+
+            // Initialize the progress bar.
+            statusbar.Progress(ref cookie, 1, "", 0, 0);
+
+            for (uint i = 1, n = (uint)projects.Count; i <= n; ++i)
             {
                 var project = projects.Item(i);
-                System.Console.WriteLine("[{0} {1}", i, project.Name);
+                statusbar.Progress(ref cookie, 1, "", i + 1, n);
+                statusbar.SetText(string.Format("Converting {0}", project.Name));
             }
+
+            // Clear the progress bar.
+            statusbar.Progress(ref cookie, 0, "", 0, 0);
+            statusbar.FreezeOutput(0);
+            statusbar.Clear();
         }
     }
 }
